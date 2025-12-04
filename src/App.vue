017bdc63f75c083805bf063ed666a01170410398
thegr <tod.jerson.morocho@gmail.com>
2025-12-04 09:51:16 -0500

    Refactor App.vue to utilize PrintView component for displaying grouped questions and remove redundant print styles

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,7 @@
       :questions="questions"
       :searched="searched"
     />
-    <div v-if="showAllCards" class="print-view">
-      <div
-        v-for="(group, index) in groupedQuestions"
-        :key="index"
-        class="print-page"
-      >
-        <Card v-for="q in group" :key="q.id" :question="q" />
-      </div>
-    </div>
+    <PrintView v-if="showAllCards" :grouped-questions="groupedQuestions" />
   </div>
 </template>
 
@@ -30,12 +22,14 @@
 import Papa from "papaparse";
 import HeaderBar from "./components/HeaderBar.vue";
 import CardsGrid from "./components/CardsGrid.vue";
+import PrintView from "./components/PrintView.vue";
 import Card from "./components/Card.vue";
 
 export default {
   components: {
     HeaderBar,
     CardsGrid,
+    PrintView,
     Card,
   },
   data() {
@@ -109,40 +103,6 @@ export default {
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-}
-
-.print-view {
-  padding: 20px;
-  box-sizing: border-box;
-  width: 100%;
-}
-
-.print-page {
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  gap: 5px;
-  margin-bottom: 20px;
-}
-
-@media print {
-  .print-page {
-    page-break-after: always;
-  }
-  .card-content {
-    height: 100% !important;
-    padding: 3px 0 !important;
-  }
-  .question {
-    font-size: 14px !important;
-    margin: 5px 0 !important;
-  }
-  .category {
-    font-size: 10px !important;
-  }
-  .id {
-    font-size: 12px !important;
-  }
 }
 </style>
 
---
+++ src/components/PrintView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="print-view">
+    <div
+      v-for="(group, index) in groupedQuestions"
+      :key="index"
+      class="print-page"
+    >
+      <Card v-for="q in group" :key="q.id" :question="q" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "./Card.vue";
+
+export default {
+  components: {
+    Card,
+  },
+  props: {
+    groupedQuestions: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style scoped>
+.print-view {
+  padding: 20px;
+  box-sizing: border-box;
+  width: 100%;
+}
+
+.print-page {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 5px;
+  margin-bottom: 20px;
+}
+
+@media print {
+  .print-page {
+    page-break-after: always;
+  }
+  .card-content {
+    height: 100% !important;
+    padding: 3px 0 !important;
+  }
+  .question {
+    font-size: 14px !important;
+    margin: 5px 0 !important;
+  }
+  .category {
+    font-size: 10px !important;
+  }
+  .id {
+    font-size: 12px !important;
+  }
+}
+</style>
